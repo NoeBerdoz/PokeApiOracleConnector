aeafb1a8d8a659490321d62a29038b19b2bbe2c3
NoeBerdoz <berdoznoe@gmail.com>
2024-07-01 15:55:59 +0200

    Fetch API Data as JSON

---
+++ src/main/java/ch/nb/PokeApiFetcher.java
@@ -0,0 +1,33 @@
+package ch.nb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PokeApiFetcher {
+
+    public static String fetchPokemonData(int id) throws IOException {
+        String urlString = "https://pokeapi.co/api/v2/pokemon/" + id;
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        System.out.println("[+] Fetching: " + urlString);
+
+
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("HttpResponseCode: " + conn.getResponseCode());
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder json = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            json.append(inputLine);
+        }
+        in.close();
+        return json.toString();
+    }
+}
